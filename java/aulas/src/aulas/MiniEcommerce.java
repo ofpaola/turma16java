71e5421e3f57cf82e58af8fa094f24f4d77a9a1e
unknown <contatofpaola@gmail.com>
2021-02-01 19:48:01 -0300

    atualizando códigos

--- java/aulas/src/aulas/MiniEcommerce.java
+++ java/aulas/src/aulas/MiniEcommerce.java
@@ -7,6 +7,7 @@ import java.math.*;
 public class MiniEcommerce {
 		public static void main(String[] args) {
 			Scanner ler = new Scanner(System.in);
+			
 			final int n = 10;
 			String[] codigoProdutos = {"01", "02", "03", "04", "05", "06", "07", "08", "09","10"};
 			String[] nomeProdutos = {"Bolinha de morder para cães",
@@ -30,49 +31,58 @@ public class MiniEcommerce {
 				int carrinhoCompras[] = new int[n];
 				String nome;
 				char sexo;
-				linha(60);
-				System.out.print("PAGUFE Pet\nAqui, seu animal é mais feliz! \n");
-				linha(60);
-				System.out.print("Informe seu nome: ");
+				
+				linha(32);
+				System.out.print("\nPAGUFE Pet\nAqui, seu animal é mais feliz! \n");
+				linha(32);
+				System.out.print("\nInforme seu nome: ");
 				nome = ler.nextLine();
-				System.out.print("Informe seu sexo [M/F/O] ");
+				System.out.print("Informe seu sexo [M/F/O]: ");
 				sexo = ler.next().toUpperCase().charAt(0);
+				
+				
 				while (!(sexo == 'M' || sexo == 'F' || sexo == 'O'))
 				{
-					System.out.print("Informe seu sexo [M/F/O] ");
+					System.out.print("Informe seu sexo [M/F/O]: ");
 					sexo = ler.next().toUpperCase().charAt(0);
-				}
-					linha(60);
-					imprimeLista(n,codigoProdutos,nomeProdutos,estoque,precoProdutos);
-					linha(60);
+				}	
+					
 				do {
-					System.out.print("\nCOMANDOS\n - a - adicionar produto ao carrinho\n - f - finalizar compra\n - s - sair");
+					linha(90);
+					imprimeLista(n,codigoProdutos,nomeProdutos,estoque,precoProdutos);
+					linha(90);
+					System.out.print("\nCOMANDOS:\na = Adicionar produto ao carrinho\nr = Retirar produto do carrinho\nc = Exibir o carrinho de compras\nf = Finalizar compra\ns = Sair\n");
+					System.out.print("\nDigite o que deseja: ");
 					comando = ler.next().toLowerCase().charAt(0);
-				if (comando == 'a') {
+				
+					if (comando == 'a') {
 					System.out.print("\nCÓDIGO DO PRODUTO: ");
 					codProduto = ler.nextLine();
 					codProduto = ler.nextLine();
 					int indice = encontraIndice(codProduto, n, codigoProdutos);
 					if (indice > -1)
+					
 				{
-					do 
+				
+				do 
 				{
 					System.out.print("\nQUANTIDADE DO PRODUTO: ");
 					qtdProduto = ler.nextInt();
 				    boolean result = adicionaAoCarrinho(indice, codProduto, qtdProduto, carrinhoCompras, n, codigoProdutos, estoque, precoProdutos);
-					if (result)
+				
+				if (result)
 				{
-					System.out.print("\nPRODUTO ADICIONADO AO CARRINHO\n\n");
+					System.out.print("\n***PRODUTO ADICIONADO AO CARRINHO***\n\n");
 					comando = 'n';
 				}
-					else
+				else
 				{
-					if (estoque[indice] > 0)
+				if (estoque[indice] > 0)
 				{
 					System.out.print("\nHÁ APENAS "+estoque[indice]+" UNIDADES DISPONÍVEIS\nDESEJA ADICIONAR OUTRA QUANTIDADE? [s/n]: ");
 					comando = ler.next().toLowerCase().charAt(0);
 				}
-					else System.out.print("\nPRODUTO ESGOTADO\n");
+				else System.out.print("\nPRODUTO ESGOTADO\n");
 										}
 									}
 									while (comando == 's');
@@ -82,25 +92,77 @@ public class MiniEcommerce {
 									System.out.print("\n\n\n---CÓDIGO DO PRODUTO NÃO ENCONTRADO---\n");
 								}
 							}
+					else if (comando == 'r') {
+						System.out.print("\nCÓDIGO DO PRODUTO: ");
+						ler.nextLine();
+						codProduto = ler.nextLine();
+						int indice = encontraIndice(codProduto, n, codigoProdutos);
+						if (indice > -1)
+						{
+							do 
+							{
+								System.out.print("\nQUANTIDADE A REMOVER: ");
+								qtdProduto = ler.nextInt();
+								
+					boolean result = retiraDoCarrinho(indice, codProduto, qtdProduto, carrinhoCompras, n, codigoProdutos, estoque);
+								if (result)
+								{
+									System.out.print("\nPRODUTO(S) RETIRADO(S) DO CARRINHO\n\n");
+									comando = 'n';
+								}
+								else
+								{
+									if (carrinhoCompras[indice] > 0)
+									{
+										System.out.print("\nHÁ APENAS"+carrinhoCompras[indice]+" UNIDADES EM SEU CARRINHO\nDESEJA REMOVER OUTRA QUANTIDADE? [s/n]: ");
+										comando = ler.next().toLowerCase().charAt(0);
+									}
+									else System.out.print("\nNÃO HÁ PRODUTOS DESTE TIPO NO SEU CARRINHO\n");
+								}
+							}
+							while (comando == 's');
+						}
+						else 
+						{
+							System.out.print("\n\n\n---CÓDIGO DO PRODUTO NÃO ENCONTRADO---\n");
+						}
+					} else if(comando == 'c')
+					{
+						System.out.print("\n\n|\tCOD\t|\tPreço\t|   Seu Carrinho   |\tProduto Restante\t\n");
+						for(int i=0;i<10;i++)
+						{
+							if(carrinhoCompras[i] != 0){
+								System.out.print("|\t"+(i+1)+"\t|\t"+precoProdutos[i]+"\t|\t"+carrinhoCompras[i]+"\t|\t"+estoque[i]+"\n");
+							}
+						}
+					}
 							else if(comando == 's')
 							{
 								break;
 							}
 							else if(comando == 'f')
 							{
-								pagamento( estoque,carrinhoCompras,  precoProdutos, nome, sexo);
-								break;
+								boolean result = pagamento( estoque,carrinhoCompras,  precoProdutos, nome, sexo);
+								if (result) break;
+								else {
+									linha(90);
+									System.out.println("\n**NÃO FOI POSSÍVEL FINALIZAR A COMPRA, POIS O CARRINHO ESTA VAZIO**\n");
+								}
 							}
 						}
 						while (true);
-						linha(60);
+						linha(50);
 						System.out.println("\nDESEJA SAIR DO PROGRAMA? [s/n]");
 						comandoExterno = ler.next().toLowerCase().charAt(0);
+						ler.nextLine();
 					}
+			
 					while (comandoExterno == 'n');
 					System.out.println("\nPROGRAMA FINALIZADO");
 				}
-				public static void linha(int tamanho) 
+		
+			public static void linha(int tamanho) 
+				
 				{
 					for (int x=1;x< tamanho; x++)
 					{
@@ -108,14 +170,18 @@ public class MiniEcommerce {
 					}
 					System.out.println();
 				}
-				static void imprimeLista(int n, String[] codigoProdutos, String[] nomeProdutos, int[] estoque, double[] precoProdutos)
+				
+			static void imprimeLista(int n, String[] codigoProdutos, String[] nomeProdutos, int[] estoque, double[] precoProdutos)
+				
 				{
 					System.out.print("\nCOD PRODUTO\tESTOQUE \tPREÇO UNITÁRIO \t NOME DO PRODUTO\n");
 					for (int i = 0; i < n; ++i) {
 						System.out.print(codigoProdutos[i]+" \t\t "+estoque[i]+" \t\t"+precoProdutos[i]+" \t\t "+nomeProdutos[i]+"\n");
 					}
 				}
-				static int encontraIndice(String codProduto, int n, String[] codigoProdutos)
+				
+			static int encontraIndice(String codProduto, int n, String[] codigoProdutos)
+				
 				{
 					int indice = -1;
 					for (int i = 0; i < n; ++i) {
@@ -126,7 +192,8 @@ public class MiniEcommerce {
 					}
 					return indice;
 				}
-				static boolean adicionaAoCarrinho(int indice, String codProduto, int qtdProduto, int carrinhoCompras[], int n, String codigoProdutos[], int estoque[], double precoProdutos[]) {
+				
+			static boolean adicionaAoCarrinho(int indice, String codProduto, int qtdProduto, int carrinhoCompras[], int n, String codigoProdutos[], int estoque[], double precoProdutos[]) {
 					if (qtdProduto < 0) return false; 
 					int restante = estoque[indice]-qtdProduto;
 					if (restante < 0) return false; 
@@ -134,7 +201,18 @@ public class MiniEcommerce {
 					estoque[indice] = estoque[indice]-qtdProduto; 
 					return true;
 				}
-				static void pagamento( int []estoque, int []carrinhoCompras, double [] precoProdutos, String nome, char sexo)
+			
+			static boolean retiraDoCarrinho(int indice, String codProduto, int qtdProduto, int carrinhoCompras[], int n, String codigoProdutos[], int estoque[]) {
+				if (qtdProduto < 0) return false;
+				int restante = carrinhoCompras[indice]-qtdProduto;
+				if (restante < 0) return false;
+				estoque[indice] = estoque[indice]+qtdProduto;
+				carrinhoCompras[indice] = carrinhoCompras[indice]-qtdProduto;
+				return true;
+			}
+				
+			static boolean pagamento( int []estoque, int []carrinhoCompras, double [] precoProdutos, String nome, char sexo)
+				
 				{
 					Scanner leia = new Scanner(System.in);
 					String formaPagamento,tratamento = "a";
@@ -150,25 +228,30 @@ public class MiniEcommerce {
 					if(sexo=='O') {
 						tratamento = "Srx." ;
 					}
-					System.out.print("\n\n|COD\t\t|\tPreço\t|    Carrinho   |\tProduto\t\n");
+					System.out.print("\n\n|\tCOD\t|\tPreço\t|   Seu Carrinho   |\tProduto Restante\t\n");
 					for(int i=0;i<10;i++)
 					{
 						if(carrinhoCompras[i] != 0){
 						System.out.print("|\t"+(i+1)+"\t|\t"+precoProdutos[i]+"\t|\t"+carrinhoCompras[i]+"\t|\t"+estoque[i]+"\n");
-						totalGeral += carrinhoCompras[i]*precoProdutos[i];
+						totalGeral = carrinhoCompras[i]*precoProdutos[i];
 						}
 					}
+					
+					if (totalGeral == 0) { //se o totalGeral é zero, o carrinho está vazio, então retorna falso
+						return false;
+					}
+					
 					totalComImposto = totalGeral+(totalGeral*0.09);
 					System.out.printf("\nTotal Geral: R$ %.2f + IMPOSTO (9%%): R$ %.2f  Total com imposto: R$ %.2f \n",totalGeral,totalGeral*0.09,totalComImposto);
 					System.out.printf("\nEscolha a forma de pagamento\n");
 					System.out.print("\n1- ZERAR CARRINHO");
 					System.out.printf("\n2- A VISTA - 10%% DESCONTO: R$%.2f",(totalComImposto*0.9));	
 					System.out.printf("\n3- CARTÃO - 1 VEZ:  R$%.2f SEM DESCONTO", totalComImposto);
-					System.out.printf("\n4- CARTÃO - 2 VEZES - JUROS (10%%) - PARCELAS DE:  R$%.2f - TOTAL DE: R$%.2f",((totalGeral+(totalGeral*0.10))/2),(totalComImposto*1.10));
-					System.out.printf("\n5- CARTÃO - 3 VEZES - JUROS (15%%) - PARCELAS DE:  R$%.2f - TOTAL DE: R$%.2f",((totalGeral+(totalGeral*0.15))/3),(totalComImposto*1.15));
-					System.out.println("\n\nInsira Aqui: ");
+					System.out.printf("\n4- CARTÃO - 2 VEZES - JUROS (10%%) - PARCELAS DE:  R$%.2f - TOTAL DE: R$%.2f",((totalComImposto+(totalComImposto*0.10))/2),(totalComImposto*1.10));
+					System.out.printf("\n5- CARTÃO - 3 VEZES - JUROS (15%%) - PARCELAS DE:  R$%.2f - TOTAL DE: R$%.2f",((totalComImposto+(totalComImposto*0.15))/3),(totalComImposto*1.15));
+					System.out.print("\n\nInsira Aqui: ");
 					opcao = leia.nextInt();
-					linha(60);
+					linha(80);
 					if(opcao == 1){
 						for(int i=0;i<10;i++)
 						{
@@ -179,16 +262,16 @@ public class MiniEcommerce {
 					}
 					else if(opcao == 2){
 						formaPagamento = "A VISTA";
-						System.out.println(" NOTA FISCAL - PAGUFE PET - CNPJ XXXXXXXXX ");
+						System.out.println("\n NOTA FISCAL - PAGUFE PET - CNPJ XXXXXXXXX ");
 						System.out.printf(" Olá %s %s Você efetuou uma compra na opção %s \n",tratamento,nome,formaPagamento);
 						for(int i=0;i<10;i++)
 						{
 							if(carrinhoCompras[i] != 0){
 							System.out.print("|\t"+(i+1)+"\t|\t"+precoProdutos[i]+"\t|\t"+carrinhoCompras[i]+"\t|\t"+estoque[i]+"\n");
-							totalGeral += carrinhoCompras[i]*precoProdutos[i];
+							
 							}
 						}
-						System.out.printf("\nTotal Geral: R$ %.2f + IMPOSTO (9%%): R$ %.2f  Total com imposto: R$ %.2f \n",totalGeral,totalGeral*0.09,totalComImposto);
+						System.out.printf("\nTotal a pagar: À vista c/ 10%% DESCONTO: R$%.2f\\n",totalComImposto*0.9);
 					}
 					else if(opcao == 3){
 						formaPagamento = "1 VEZ NO CARTÃO";
@@ -198,10 +281,10 @@ public class MiniEcommerce {
 						{
 							if(carrinhoCompras[i] != 0){
 							System.out.print("|\t"+(i+1)+"\t|\t"+precoProdutos[i]+"\t|\t"+carrinhoCompras[i]+"\t|\t"+estoque[i]+"\n");
-							totalGeral += carrinhoCompras[i]*precoProdutos[i];
+					
 							}
 						}
-						System.out.printf("\nTotal Geral: R$ %.2f + IMPOSTO (9%%): R$ %.2f  Total com imposto: R$ %.2f \n",totalGeral,totalGeral*0.09,totalComImposto);
+						System.out.printf("\nTotal a pagar: 1 vez no cartão s/ Desconto: R$%.2f\n",totalComImposto);
 					}
 					else if(opcao == 4){
 						formaPagamento = "2 VEZES NO CARTÃO";
@@ -211,10 +294,10 @@ public class MiniEcommerce {
 						{
 							if(carrinhoCompras[i] != 0){
 							System.out.print("|\t"+(i+1)+"\t|\t"+precoProdutos[i]+"\t|\t"+carrinhoCompras[i]+"\t|\t"+estoque[i]+"\n");
-							totalGeral += carrinhoCompras[i]*precoProdutos[i];
+							
 							}
 						}
-						System.out.printf("\nTotal Geral: R$ %.2f + IMPOSTO (9%%): R$ %.2f  Total com imposto: R$ %.2f \n",totalGeral,totalGeral*0.09,totalComImposto);
+						System.out.printf("\nTotal a pagar: 2 VEZES + JUROS (10%%) - PARCELAS DE:  R$%.2f - TOTAL DE: R$%.2f \n",((totalComImposto+(totalComImposto*0.10))/2),(totalComImposto*1.10));
 					}
 					else if(opcao == 5){
 						formaPagamento = "3 VEZES NO CARTÃO";
@@ -224,13 +307,15 @@ public class MiniEcommerce {
 						{
 							if(carrinhoCompras[i] != 0){
 							System.out.print("|\t"+(i+1)+"\t|\t"+precoProdutos[i]+"\t|\t"+carrinhoCompras[i]+"\t|\t"+estoque[i]+"\n");
-							totalGeral += carrinhoCompras[i]*precoProdutos[i];
+							
 							}
 						}
-						System.out.printf("\nTotal Geral: R$ %.2f + IMPOSTO (9%%): R$ %.2f  Total com imposto: R$ %.2f \n",totalGeral,totalGeral*0.09,totalComImposto);
+						System.out.printf("\nTotal a pagar: 3 VEZES + JUROS (15%%) - PARCELAS DE: R$%.2f - TOTAL DE: R$%.2f \n",((totalComImposto+(totalComImposto*0.15))/3),(totalComImposto*1.15));
 					}else{
 						System.out.print("\nOpção inválida!\nTente novamente");
 					}
 					System.out.println("Agrademos pela compra. Volte sempre !");
+					
+					return true;
 				}
 			}
