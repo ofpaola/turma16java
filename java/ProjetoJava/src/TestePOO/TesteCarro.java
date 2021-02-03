57ed086206ced528bd93fbf95d0d982fdb6bfedf
unknown <contatofpaola@gmail.com>
2021-02-02 23:01:54 -0300

    atualizando códigos

---
+++ java/ProjetoJava/src/TestePOO/TesteCarro.java
@@ -0,0 +1,20 @@
+package TestePOO;
+
+public class TesteCarro {
+
+	public static void main(String[] args) {
+		
+		Carro meuCarro = new Carro();
+		meuCarro.cor = "Preto";
+		meuCarro.modelo = "Passat";
+		meuCarro.velocidadeAtual = 0;
+		meuCarro.velocidadeMaxima = 80;
+		
+		meuCarro.liga();
+		
+		meuCarro.acelera(20);
+		System.out.println(meuCarro.velocidadeAtual);
+		meuCarro.pegaMarcha();
+	}
+
+}
