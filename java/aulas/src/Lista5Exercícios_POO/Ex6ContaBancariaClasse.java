e4b87b685ed5312023b42249214d7df89b481c58
unknown <contatofpaola@gmail.com>
2021-02-02 23:58:12 -0300

    adicionando novos códigos

--- java/aulas/src/Lista5Exercícios_POO/Ex6ContaBancariaClasse.java
+++ java/aulas/src/Lista5Exercícios_POO/Ex6ContaBancariaClasse.java
@@ -3,7 +3,51 @@ package Lista5Exercícios_POO;
 public class Ex6ContaBancariaClasse {
 		
 		String titular;
+		int numeroConta;
 		int cpf;
 		double saldo;
+		double limite;
 		
+		public String titular() {
+			return titular;
+		}
+		public void titular (String titular) {
+			this.titular = titular;
+		}
+		
+		public int numeroConta() {
+			return numeroConta;
+		}
+		public void numeroConta (int numeroConta) {
+			this.numeroConta = numeroConta;
+		}
+		
+		public int cpf() {
+			return cpf;
+		}
+		public void cpf (int cpf) {
+			this.cpf = cpf;
+		}
+		
+		public double saldo() {
+			return saldo;
+		}
+		public void saldo (double saldo) {
+			this.saldo = saldo;
+		}
+		
+		public double limite() {
+			return limite;
+		}
+		public void limite (double limite) {
+			this.limite = limite;
+		}
+
+		public Ex6ContaBancariaClasse (String titular, int numeroConta, int cpf, double saldo, double limite) {
+			this.titular = titular;
+			this.numeroConta = numeroConta;
+			this.cpf = cpf;
+			this.saldo = saldo;
+			this.limite = limite;
+		}
 }
--- java/aulas/src/Lista5Exercícios_POO/Ex6ContaBancariaObjeto.java
+++ java/aulas/src/Lista5Exercícios_POO/Ex6ContaBancariaObjeto.java
@@ -1,5 +1,17 @@
 package Lista5Exercícios_POO;
 
 public class Ex6ContaBancariaObjeto {
+	public static void main(String[] args) {
+		
+		Ex6ContaBancariaClasse conta = new Ex6ContaBancariaClasse("Paola Alencar",0202,01010101010,2000,20000);
+		
+		System.out.println("Titular\t\tCPF\t\tNúmero da Conta\tSaldo\tLimite");
+		System.out.println(conta.titular+"\t"
+						+conta.cpf+"\t"
+						+conta.numeroConta+"\t\t"
+						+conta.saldo+"\t"
+						+conta.limite);
+		
+	}
 
 }
