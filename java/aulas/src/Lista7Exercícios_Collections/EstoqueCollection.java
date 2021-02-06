855438cf5ff107a3cfedc8015dee06269bbca0bd
unknown <contatofpaola@gmail.com>
2021-02-05 23:53:08 -0300

    adicionando novos códigos

--- java/aulas/src/Lista7Exercícios_Collections/EstoqueCollection.java
+++ java/aulas/src/Lista7Exercícios_Collections/EstoqueCollection.java
@@ -4,7 +4,7 @@ public class EstoqueCollection {
 		
 	private double preco;
 	private String descricao;
-	private int quantidade;
+	private int estoque;
 	
 	public double getPreco() {
 		return preco;
@@ -18,18 +18,18 @@ public class EstoqueCollection {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-	public int getQuantidade() {
-		return quantidade;
+	public int getEstoque() {
+		return estoque;
 	}
-	public void setQuantidade(int quantidade) {
-		this.quantidade = quantidade;
+	public void setEstoque(int estoque) {
+		this.estoque = estoque;
 	}
 	
-	public EstoqueCollection(double preco, String descricao, int quantidade) {
+	public EstoqueCollection(double preco, String descricao, int estoque) {
 		super();
 		this.preco = preco;
 		this.descricao = descricao;
-		this.quantidade = quantidade;
+		this.estoque = estoque;
 	}
 	
 	
--- java/aulas/src/Lista7Exercícios_Collections/EstoqueListTeste.java
+++ java/aulas/src/Lista7Exercícios_Collections/EstoqueListTeste.java
@@ -3,21 +3,52 @@ package Lista7Exercícios_Collections;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Collection;
 
 public class EstoqueListTeste {
 	public static void main(String[] args) {
 		
 		Scanner leia = new Scanner(System.in);
 		List <EstoqueCollection> lista = new ArrayList<>();
-		char opcao;
+		char opcao=0;
 		
+		while(opcao!=0) {
+			System.out.println("[PRODUTO][PREÇO][ESTOQUE]");
+			for(EstoqueCollection produtos : lista) {
+				System.out.printf("%s\t%.2f\t%d\n",produtos.getDescricao(), produtos.getPreco(), produtos.getEstoque());
+		}
+			
 		System.out.println("Digite uma opção: "
-							+"0 = Sair do programa!"
 							+"1 = Adicionar algum produto."
-							+"Remover algum produto."
-							+"Atualizar produtos.");
+							+"2 = Remover algum produto."
+							+"3 = Atualizar produtos."
+							+"0 = Sair do programa!");
 		opcao = leia.next().charAt(0);
 		
+		if(opcao==1) {
+			System.out.print("Digite o nome do produto: ");
+			String descricao = leia.next();
+			System.out.print("Digite o preço do produto: ");
+			double preco = leia.nextDouble();
+			System.out.println("Digite o estoque disponível: ");
+			int estoque = leia.nextInt();
+			EstoqueCollection estoquec = new EstoqueCollection(preco, descricao, estoque);
+			lista.add(estoquec);
+			}
+		else if(opcao==2) {
+			System.out.println("Digite o nome do produto que deseja remover: ");
+			String removePro = leia.next();
+			for (EstoqueCollection estoquec : lista) {
+				if (estoquec.getDescricao().equals(removePro))
+				{
+					lista.remove(estoquec);
+				}
+			  }
+			}
+		else if(opcao==3) {
+			
+		}
+		}
 		
 	}
 
